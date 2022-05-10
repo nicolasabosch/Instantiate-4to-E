b25f8596fee47b984cc8ce08e9d40ac3bb696e7b
nicolasabosch <nicolasabosch@gmail.com>
2022-05-10 15:38:02 -0300

    Update dataInputManager.cs

--- Assets/Scripts/dataInputManager.cs
+++ Assets/Scripts/dataInputManager.cs
@@ -6,9 +6,19 @@ using UnityEngine.UI;
 public class dataInputManager : MonoBehaviour
 {
     public Text txtUsrName;
-    
+
     public void ShowGreetings()
     {
-        Debug.Log ("Hola" + txtUsrName.text);
+        Debug.Log("Hola " + txtUsrName.text);
+    }
+
+    public void ShowDouble()
+    {
+
+        if (txtUsrName.text != "")
+        {
+
+            Debug.Log(int.Parse(txtUsrName.text) * 2);
+        }
     }
 }
