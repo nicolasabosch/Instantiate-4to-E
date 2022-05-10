70726aab8c015d5e7c5ca16ccc1fcb2951253b79
nicolasabosch <nicolasabosch@gmail.com>
2022-05-10 15:55:21 -0300

    muchas cosas

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -1,28 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
+    public int cloneAmount;
+    public Text txtUserCloneAmount;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    cloneAmount = int.Parse(txtUserCloneAmount.text);
 
-    public void TripleInstantiate()
+
+    public void MultiInstantiate()
     {
         int counter = 0;
-        while (counter < 3)
+        while (counter < cloneAmount)
         {
             Instantiate(objectToClone);
             counter++;
